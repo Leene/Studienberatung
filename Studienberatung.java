e392eb210a061429eac4d71d0c95f2e2b38b2b19
Leene <marleenstueber@web.de>
2014-03-26 19:35:06 +0100

    1. Frage und Scanner-Objekt hinzugefuegt

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,7 +1,11 @@
-
+import java.util.Scanner;
 public class Studienberatung {
 	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		String antwort = scanner.next();
 		
 	}
 } 
